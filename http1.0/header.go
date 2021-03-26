b32a3a1f4c7afc7ff48fc5f42bdad72b3a5f7abb
林　遼太朗 <hys.rtr.forit@gmail.com>
2021-03-26 13:35:49 +0900

    http/1.0でのhttp clientの実装例

---
+++ http1.0/header.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, _ := http.Head("http://localhost:18888")
+	log.Println("Status:", resp.Status)
+	log.Println("Headers:", resp.Header)
+}
---
+++ http1.0/proxy.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	proxyUrl, _ := url.Parse("http://localhost:18888")
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+	resp, _ := client.Get("http://github.com")
+	dump, _ := httputil.DumpResponse(resp, true)
+	log.Println(string(dump))
+}
